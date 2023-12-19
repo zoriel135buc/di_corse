51594afa3e80780ab5a1fbd8abacb4e8db85e996
zoriel135buc <146331116+zoriel135buc@users.noreply.github.com>
2023-12-19 10:05:36 +0200

    Delete week5/day2/exercise3 directory

--- week5/day2/exercise3/script.js
+++
@@ -1,7 +0,0 @@
-const newDiv = document.getElementById("navBar");
-newDiv.setAttribute("id", "socialNetworkNavigation");
-
-const newLi = document.createElement("li");
-const textLI = document.createTextNode("Logout");
-newLi.appendChild(textLI);
-document.getElementById("socialNetworkNavigation").appendChild(newLi);
